d90da632f815b64c4a870551d65a6404b481a96a
yves <yvmaison@free.fr>
2020-11-07 21:24:01 +0100

    reception sans cde V1

---
+++ Tuto_Reception.js
@@ -0,0 +1,79 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 20,
+  buttonFrameLength: 10,
+  buttonFrameOffset: 5,
+  frameStops: {
+    "607": [
+      { type: "gotoframe", rect: { x: 1127, y: 668, width: 60, height: 24 }, target: 608 },
+    ],
+    "2810": [
+      { type: "gotoframe", rect: { x: 1107, y: 679, width: 56, height: 19 }, target: 2811 },
+      { type: "gotoframe", rect: { x: 1027, y: 676, width: 60, height: 24 }, target: 607 },
+      { type: "gotoframe", rect: { x: 957, y: 673, width: 60, height: 24 }, target: 15603 },
+    ],
+    "3538": [
+      { type: "gotoframe", rect: { x: 1121, y: 673, width: 60, height: 24 }, target: 3539 },
+    ],
+    "4493": [
+      { type: "gotoframe", rect: { x: 1108, y: 674, width: 60, height: 24 }, target: 4494 },
+      { type: "gotoframe", rect: { x: 1033, y: 672, width: 60, height: 24 }, target: 3538 },
+      { type: "gotoframe", rect: { x: 962, y: 672, width: 60, height: 24 }, target: 15603 },
+    ],
+    "4658": [
+      { type: "gotoframe", rect: { x: 1115, y: 676, width: 60, height: 24 }, target: 4659 },
+      { type: "gotoframe", rect: { x: 1023, y: 676, width: 60, height: 24 }, target: 4493 },
+      { type: "gotoframe", rect: { x: 961, y: 672, width: 60, height: 24 }, target: 15603 },
+    ],
+    "5064": [
+      { type: "gotoframe", rect: { x: 1112, y: 672, width: 60, height: 24 }, target: 5065 },
+      { type: "gotoframe", rect: { x: 1031, y: 676, width: 60, height: 24 }, target: 4658 },
+      { type: "gotoframe", rect: { x: 961, y: 672, width: 60, height: 24 }, target: 15603 },
+    ],
+    "7026": [
+      { type: "gotoframe", rect: { x: 1140, y: 676, width: 60, height: 24 }, target: 7027 },
+      { type: "gotoframe", rect: { x: 1053, y: 673, width: 60, height: 24 }, target: 5064 },
+      { type: "gotoframe", rect: { x: 961, y: 672, width: 60, height: 24 }, target: 15603 },
+    ],
+    "7808": [
+      { type: "gotoframe", rect: { x: 1128, y: 676, width: 60, height: 24 }, target: 7809 },
+      { type: "gotoframe", rect: { x: 1056, y: 676, width: 60, height: 24 }, target: 7026 },
+      { type: "gotoframe", rect: { x: 961, y: 672, width: 60, height: 24 }, target: 15603 },
+    ],
+    "9847": [
+      { type: "gotoframe", rect: { x: 1134, y: 670, width: 60, height: 24 }, target: 9848 },
+      { type: "gotoframe", rect: { x: 1054, y: 670, width: 60, height: 24 }, target: 7808 },
+      { type: "gotoframe", rect: { x: 961, y: 672, width: 60, height: 24 }, target: 15603 },
+    ],
+    "10362": [
+      { type: "gotoframe", rect: { x: 1093, y: 670, width: 60, height: 24 }, target: 10363 },
+      { type: "gotoframe", rect: { x: 1013, y: 670, width: 60, height: 24 }, target: 9847 },
+      { type: "gotoframe", rect: { x: 961, y: 672, width: 60, height: 24 }, target: 15603 },
+    ],
+    "11206": [
+      { type: "gotoframe", rect: { x: 1118, y: 669, width: 60, height: 24 }, target: 11207 },
+      { type: "gotoframe", rect: { x: 1042, y: 671, width: 60, height: 24 }, target: 10362 },
+      { type: "gotoframe", rect: { x: 961, y: 672, width: 60, height: 24 }, target: 15603 },
+    ],
+    "12186": [
+      { type: "gotoframe", rect: { x: 1124, y: 670, width: 60, height: 24 }, target: 12187 },
+      { type: "gotoframe", rect: { x: 1029, y: 667, width: 60, height: 24 }, target: 11206 },
+      { type: "gotoframe", rect: { x: 961, y: 672, width: 60, height: 24 }, target: 15603 },
+    ],
+    "13380": [
+      { type: "gotoframe", rect: { x: 1114, y: 670, width: 60, height: 24 }, target: 13381 },
+      { type: "gotoframe", rect: { x: 1040, y: 670, width: 60, height: 24 }, target: 12186 },
+      { type: "gotoframe", rect: { x: 961, y: 672, width: 60, height: 24 }, target: 15603 },
+    ],
+    "14215": [
+      { type: "gotoframe", rect: { x: 1130, y: 670, width: 60, height: 24 }, target: 14216 },
+      { type: "gotoframe", rect: { x: 1044, y: 667, width: 60, height: 24 }, target: 13380 },
+      { type: "gotoframe", rect: { x: 961, y: 672, width: 60, height: 24 }, target: 15603 },
+    ],
+    "15593": [
+      { type: "gotoframe", rect: { x: 1129, y: 667, width: 60, height: 24 }, target: 15594 },
+      { type: "gotoframe", rect: { x: 1038, y: 667, width: 60, height: 24 }, target: 14215 },
+      { type: "gotoframe", rect: { x: 961, y: 672, width: 60, height: 24 }, target: 15603 },
+    ],
+  },
+};
